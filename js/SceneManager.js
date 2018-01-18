ee9a90d399ebc8bfb77ba4c027b50960e73a7a92
Stanley Su <stanleysu00@gmail.com>
2018-01-19 00:45:32 +1100

    updated camera position in accordance to new sea object dimensions

--- js/SceneManager.js
+++ js/SceneManager.js
@@ -25,8 +25,8 @@ function SceneManager(canvas) {
     const nearPlane = 1
     const farPlane = 10000
     const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane)
-    camera.position.set(-200, 250, 0)
-    camera.lookAt(new THREE.Vector3(0, 100, 0))
+    camera.position.set(-250, 2100, 0)
+    camera.lookAt(new THREE.Vector3(0, 2100, 0))
 
     return camera
   }
